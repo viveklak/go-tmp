b8bdb43511ccd4f0008bcb03acb9933ca66f6e7f
Vivek Lakshmanan <vivek@pulumi.com>
2021-04-30 15:06:25 -0700

    Use vault provider

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"io/ioutil"
+
+	"github.com/pulumi/pulumi-vault/sdk/v4/go/vault"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		return nil
+		vaultProvider, err := vault.NewProvider(ctx, "vault", &vault.ProviderArgs{
+			Address: pulumi.String("https://myvaultaddr"),
+            Token: pulumi.ToSecret(pulumi.String("abc")).(pulumi.StringOutput),
+		})
+		if err != nil {
+			return err
+		}
+
+		// configure test kv backend
+		_, err = vault.NewMount(ctx, "operator_test_kv", &vault.MountArgs{
+			Description: pulumi.String("This is a test of pulumi operator"),
+			Path:        pulumi.String("operator/kv"),
+			Type:        pulumi.String("kv"),
+		}, pulumi.Provider(vaultProvider))
+
+		return err
 	})
 }
+
